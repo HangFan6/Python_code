86ca995be9a964715a9e1b8545e0fdce884902f9
HangFan6 <2691567181@qq.com>
2023-01-31 18:21:25 +0800

    打鱼晒网问题

---
+++ 打鱼晒网问题/test1_31.py
@@ -0,0 +1,45 @@
+# -*- coding:utf-8 -*-
+"""
+作者：HET
+日期：2023年01月31日
+"""
+year = int(input("请输入查询Year："))
+month = int(input("请输入查询Month："))
+date = int(input("请输入查询Date："))
+
+
+# 闰年规则：四年一闰,百年不闰,四百年再闰
+# 判断闰年
+def is_leapyear(year):
+    if year % 4 == 0 and year % 100 != 0 or year % 400 == 0:
+        return 1
+    else:
+        return 0
+
+
+# 计算从2000年到year-1年的天数
+y_days = 0
+for i in range(2000, year):
+    if is_leapyear(i):
+        y_days = y_days + 366
+    else:
+        y_days = y_days + 365
+print(y_days)
+# 计算year年1月到month月的天数
+months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+m_days = 0
+i = month - 1
+for i in range(0, i):
+    m_days = m_days + months[i]
+    if is_leapyear(year) and month > 2:
+        m_days += 1
+print(m_days)
+# 计算2000年1月1日 到 xx年xx月xx日 的天数
+sum_all = y_days + m_days + date
+print(sum_all)
+# 计算小王打鱼还是晒网
+x = sum_all % 5
+if x in range(1, 4):
+    print("打鱼去喽")
+else:
+    print("晒网去喽")  # x=0或4
