999b2cddba7dfd686930ff30db0160ecb98b7924
HangFan6 <2691567181@qq.com>
2023-04-08 16:07:20 +0800

    普通练习

--- 练习题/test3_30.py
+++ 练习题/test3_30.py
@@ -255,48 +255,120 @@
 #     print(time.time()-start)
 #     print('parent is %s'% os.getpid())
 
-import os
-import asyncio
-import time
-import random
-import gevent
+# import os
+# import asyncio
+# import time
+# import random
+# import gevent
+#
+# def gevent_a():
+#     for i in range(10):
+#         print(i,'a gevent',os.getpid())
+#         gevent.sleep(random.random() * 2)
+#     return 'gevent a result'
+# def gevent_b():
+#     for i in range(10):
+#         print(i,'b gevent',os.getpid())
+#         gevent.sleep(random.random() * 2)
+#     return 'gevent b result'
+#
+# async def a():
+#     for i in range(10):
+#         print(i,'a',os.getpid())
+#         await asyncio.sleep(random.random() * 2)
+#     return 'a function'
+# async def b():
+#     for i in range(10):
+#         print(i,'b',os.getpid())
+#         await asyncio.sleep(random.random()*2)
+#     return 'b function'
+# async def main():
+#     result=await asyncio.gather(a(),b())
+#     print(result)
+#
+# if __name__=='__main__':
+#     start=time.time()
+#     g_a=gevent.spawn(gevent_a)
+#     g_b=gevent.spawn(gevent_b)
+#     gevent_list=[g_a,g_b]
+#     result=gevent.joinall(gevent_list)
+#     print(result)
+#
+#     print(time.time()-start)
+#     print('parent is %s'% os.getpid())
 
-def gevent_a():
-    for i in range(10):
-        print(i,'a gevent',os.getpid())
-        gevent.sleep(random.random() * 2)
-    return 'gevent a result'
-def gevent_b():
-    for i in range(10):
-        print(i,'b gevent',os.getpid())
-        gevent.sleep(random.random() * 2)
-    return 'gevent b result'
 
-async def a():
-    for i in range(10):
-        print(i,'a',os.getpid())
-        await asyncio.sleep(random.random() * 2)
-    return 'a function'
-async def b():
-    for i in range(10):
-        print(i,'b',os.getpid())
-        await asyncio.sleep(random.random()*2)
-    return 'b function'
-async def main():
-    result=await asyncio.gather(a(),b())
-    print(result)
+# ================正则表达式===========
+import re
 
-if __name__=='__main__':
-    start=time.time()
-    g_a=gevent.spawn(gevent_a)
-    g_b=gevent.spawn(gevent_b)
-    gevent_list=[g_a,g_b]
-    result=gevent.joinall(gevent_list)
-    print(result)
+# data='dewei@headwork.com'
+# result=re.findall('\w{4,6}',data)
+# print(result)
 
-    print(time.time()-start)
-    print('parent is %s'% os.getpid())
+# test='hello my name is dewei'
+# result=re.search('hello (.*) name is (.*)',test)
+# print(result.groups())
+# print(result.group(1))
+# print(result.group(2))
 
+# str_1 = 'I don\'t want to be your entire world, just the best thing in it, I want to say "$%^&*@#~!".'
+# str_2 = "12i3 love 34you3 not 56because of 7who3 119  df44"
+# print(re.findall('["].*["]',str_1))
+# print(re.findall('"(.*)"',str_1))
+#
+# print(re.findall('\d\w+\d',str_2))
+# print(re.findall('\d\w*\d',str_2))
 
+# def get_email(data):
+#     # result=re.findall('[0-9a-zA-Z]+@[0-9a-zA-Z]+\.[a-zA-Z]+',data)
+#     result=re.findall('.+@.+\.[a-zA-Z]+',data)
+#     return result
+# if __name__=='__main__':
+#     email='dewei@imooc.com'
+#     print(get_email(email))
+
+# html=('<div class="s-top-nav" style="display:none;">'
+#      '</div><div class="s-center-box"></div>')
+# def get_html_data(data):
+#     result=re.findall('style="(.*?)"',data)
+#     return result
+# def get_all_html(data):
+#     result=re.findall('="(.+?)"',data)
+#     return result
+# if __name__=='__main__':
+#     result=get_html_data(html)
+#     print(result)
+#     result=get_all_html(html)
+#     print(result)
+
+# email=['lianflower@163.com','11123433@qq.com','xinlang@sina.com']
+# def get_email(data):
+#     result=re.findall('@(.*)\.',data)
+#     return result
+# if __name__=='__main__':
+#     for i in range(len(email)):
+#         result=get_email(email[i])
+#         print(result)
+#         if result=='163':
+#             print('163邮箱')
+#         elif result=='qq':
+#             print('qq邮箱')
+#         else:
+#             print('新浪邮箱')
+
+# noodles='康师傅, 康帅傅, 康师傅, 康帅傅, 康帅傅, 康师傅, 康帅傅, 康师傅, 康帅傅, 康师傅'
+# def check(data):
+#     result=re.findall('康[^师]傅',noodles)
+#     return result
+# if __name__=='__main__':
+#     result=check(noodles)
+#     print("小慕买了%d包山寨商品"%len(result))
+
+def memory(data,l=[]):
+    l.append(data)
+    print(l)
+memory('I')
+memory('love',[])
+memory('imooc')
 
 
