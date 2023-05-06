2ac05979ec9681ec9c4ee131ec88714ff12b8620
HangFan6 <2691567181@qq.com>
2023-05-06 10:31:55 +0800

    习题

--- 练习题/数据分析/homework/test3.py
+++ 练习题/数据分析/homework/test3.py
@@ -3,3 +3,13 @@
 作者：HET
 日期：2023年05月06日
 """
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+data=np.random.randn(4,4)
+excel=pd.DataFrame(data,index=list('ABCD'),columns=list('OPKL'))
+print(excel)
+excel.plot()
+excel.plot(kind='barh')  # 横向条形图
+plt.show()
