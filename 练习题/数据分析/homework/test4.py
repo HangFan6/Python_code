e186de353eaab4b3069706b47dfa2604405d3922
HangFan6 <2691567181@qq.com>
2023-05-15 22:51:07 +0800

    习题

--- 练习题/数据分析/homework/test4.py
+++ 练习题/数据分析/homework/test4.py
@@ -13,22 +13,30 @@ rc = {'font.sans-serif': 'SimHei',   # SimHei表示黑体
 # plt.rcParams['axes.unicode_minus'] = False
 
 '''test1 观察一下五首歌曲的每日播放量变化情况'''
-example=pd.read_csv('spotify.csv',index_col='Date')
-example.index=pd.to_datetime(example.index)
-# print(example)
-bymonth=example.resample('M').first()
-print(bymonth)
-sns.set(rc=rc) # 使图表显示中文字体
-sns.lineplot(data=example)
-plt.xlim(pd.to_datetime("2017-04-01"), pd.to_datetime("2018-01-31"))
-plt.tight_layout()
-plt.title('5首流行歌曲的每日播放量（2017-2018）')
+# example=pd.read_csv('spotify.csv',index_col='Date')
+# example.index=pd.to_datetime(example.index)
+# # print(example)
+# bymonth=example.resample('M').first()
+# print(bymonth)
+# sns.set(rc=rc) # 使图表显示中文字体
+# sns.lineplot(data=example)
+# plt.xlim(pd.to_datetime("2017-04-01"), pd.to_datetime("2018-01-31"))
+# plt.tight_layout()
+# plt.title('5首流行歌曲的每日播放量（2017-2018）')
+# plt.show()
+# print(plt.rcParams['font.sans-serif'])
+
+'''test2 什么样的人群保险费用应该高一些？'''
+data=pd.read_csv('healthy_data.csv')
+# print(data)
+sns.set()
+plt.figure(1)
+sns.scatterplot(data=data,x='bmi',y='charges',hue='smoker')
+plt.figure(2)
+sns.lmplot(data=data,x='bmi',y='charges',hue='smoker')
 plt.show()
-print(plt.rcParams['font.sans-serif'])
-
-
-
-
+sns.swarmplot(data=data,x='smoker',y='charges')
+plt.show()
 
 
 
